9cd56b769120bd65a996139b6fc3c169e286a672
Christopher Dickey <cmdickey@gmail.com>
2018-03-12 21:30:21 -0700

    Removing README from setup

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from distutils.core import setup
 if version_info < (3,0):
     raise NotImplementedError('This module works only in Python 3.x versions.')
 
-with open('README.rst') as f:
-    readme = f.read()
-
 setup(
     name='python-reCAPTCHA',
     version='0.1.0',
@@ -19,5 +16,4 @@ setup(
     url='http://pypi.python.org/pypi/python-reCAPTCHA/',
     license='LICENSE.txt',
     description='Python module for reCAPTCHA service.',
-    long_description=readme,
 )
